208b18fb971e5769c8be0fb6567a8cfdf55af2a0
itramell <iona_tramelli@taylor.edu>
2022-10-26 10:20:23 -0400

    All pages have intial starts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,16 @@
 class StaticPagesController < ApplicationController
   def team
   end
+
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def help
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'static_pages/team'
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'static_pages/help'
+  get 'static_pages/team'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
