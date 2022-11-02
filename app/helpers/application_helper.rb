610fd28d819e4af23a1e99974eeef7200dbcd12f
itramell <iona_tramelli@taylor.edu>
2022-11-02 19:58:36 -0400

    All errors fixed and homework completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
 module ApplicationHelper
-<<<<<<< HEAD
-    def full_title(page_title = '')
-        basic_title = "Ruby on Rails Tutorial Sample App"
-        if page_title.empty?
-            basic_title
-        else 
-            page_title + " | " + basic_title
-        end
-    end
-=======
 
     # Returns the full title on a per-page basis.
     def full_title(page_title = '')
@@ -20,5 +10,4 @@ module ApplicationHelper
         end
     end
 
->>>>>>> working
 end
